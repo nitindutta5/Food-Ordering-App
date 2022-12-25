dccb7cd47a35ad3ee0a370f134f3749552b71bd4
nitinduttaveative <nitin.dutta@veative.com>
2022-12-25 13:30:44 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+const heading = React.createElement(
+    "h1",
+    {
+      className: "hell",
+    },
+    "Heading 1"
+  );
+  
+  const heading2 = React.createElement(
+    "h2",
+    {
+      id: "title",
+    },
+    "Heading 2"
+  );
+  
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading, heading2]
+  );
+  
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  //passing a react element inside the root
+  
+  //async defer
+  root.render(container);
