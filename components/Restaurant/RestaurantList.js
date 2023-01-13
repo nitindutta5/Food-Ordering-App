64985e58ddd63482d7b41d50a1b926a7b59f22cb
nitindutta5 <nitindutta5@gmail.com>
2023-01-13 22:49:22 +0530

    Implemented Filter

--- components/Restaurant/RestaurantList.js
+++ components/Restaurant/RestaurantList.js
@@ -1,13 +1,40 @@
 import { restaurants } from "../../data";
 import RestaurantCard from "./RestaurantCard";
-import styles from './Restaurant.module.css'
+import styles from "./Restaurant.module.css";
+import { useState } from "react";
 
 const RestaurantList = () => {
+  const [filter, setFilter] = useState("");
+  const [filteredRestaurants, setFilteredRestaurants] = useState([
+    ...restaurants,
+  ]);
+  const [loading, setLoading] = useState(false);
+  const handleFilter = (e) => {
+    setLoading(true);
+    let val = e.target.value;
+    setFilter(val);
+    let filteredData = restaurants.filter((item) =>
+      item.data.name.includes(val)
+    );
+    setFilteredRestaurants([...filteredData]);
+    setLoading(false);
+  };
   return (
-    <div className={styles.restaurantHolder + ` container`}>
-      {restaurants.map((item) => (
-        <RestaurantCard key={item.data.uuid} {...item.data}/>
-      ))}
+    <div className="container">
+      <input
+        type="text"
+        value={filter}
+        onChange={handleFilter}
+        placeholder="Search Restaurant"
+        className={styles.textField}
+      />
+      <div className={styles.restaurantHolder}>
+        {loading
+          ? "Loading..."
+          : filteredRestaurants.map((item) => (
+              <RestaurantCard key={item.data.uuid} {...item.data} />
+            ))}
+      </div>
     </div>
   );
 };
