cd37dca5c971a074ab013c642e24459dbc9031e8
nitindutta5 <nitindutta5@gmail.com>
2023-03-04 23:31:36 +0530

    Food app cart build

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import styles from "../Restaurant/Restaurant.module.css"
+import { CLOUDINARY_URL } from "../../utils/constants";
 
 const Cart = () => {
   const cart = useSelector((state) => state?.cart);
@@ -18,11 +20,44 @@ const Cart = () => {
         </>
       ) : (
         <>
-          <ul>
-            {cart?.map((item) => (
-              <li key={item.id}>{item.name}</li>
-            ))}
-          </ul>
+                <div className={styles.menu}>
+                  <ul>
+                    {cart?.map((item) => (
+                      <li key={item.id}>
+                        <div className="align-items-center d-flex justify-content-start p-2 pl-0">
+                          {item?.cloudinaryImageId ? (
+                            <img
+                              src={`${CLOUDINARY_URL}${item?.cloudinaryImageId}`}
+                              alt="rest"
+                              className={styles.smallImage}
+                            />
+                          ):<div className={styles.smallImage}>No Image</div>}
+                          <p className="mx-3 p-0">
+                            {item.name} - Rs.
+                            <b>{Math.round(item.price / 100)}</b>
+                            <br />
+                            {item?.description}
+                          </p>
+                          <button
+                            className="btn-orange p-2 ms-auto"
+                            onClick={() => dispatch(addToCart(item))}
+                          >
+                            Add
+                          </button>
+                          {
+                            cart?.some(i => i.id === item.id) && 
+                            <button
+                            className="btn-orange p-2 ms-2"
+                            onClick={() => clearCart(item)}
+                          >
+                            Remove
+                          </button>
+                          }
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
         </>
       )}
     </div>
